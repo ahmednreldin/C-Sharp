b8dfb31a48b71c03401ac532b44968188586d53d
ahmednr-dev <ahmednr.dev@gmail.com>
2021-04-25 22:03:55 +0200

    VerbatimLiteral

--- Basic/escapeSequenceAndVerbatimLiteral/escapeSequenceAndVerbatimLiteral/Program.cs
+++ Basic/escapeSequenceAndVerbatimLiteral/escapeSequenceAndVerbatimLiteral/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 
-namespace escapeSequenceAndVerbatimLiteral
-{
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        // Path Example 
+
+        // Method 1 using escape sequence / 
+        string path1 = "D:\\C#\\C-Sharp\\Basic";
+        Console.WriteLine(path1);
+
+
+        // using Verbatim Litera @ 
+        string path = @"D:\C#\C-Sharp\Basic"; // anything inside double qutetion will print
+        Console.WriteLine(path);
+
+        // Enhance Readability 
         }
     }
-}

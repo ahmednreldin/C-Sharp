b2b3321484a12c3e92b8aa9371ee3cc31f94b8f2
ahmednr-dev <ahmednr.dev@gmail.com>
2021-04-26 01:51:27 +0200

    StronglyTypedFeature

---
+++ Basic/stronglyTyped/stronglyTyped/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        // C# (and C++ and many other languages) is strongly typed
+        // because the compiler will detect and flag these errors at compilation time.
+
+        // For e.g you cannot Multiply or Divide two different types i.e String vs Integer
+        var answer = 1 * "1"; // you cannot do this
+
+        // You have to explicity cast it, this is known as strongly typed
+
+       // where as if you see in php
+      // $x = "3" * 1; // is correct in php
+      //So here you dont need to explicitly cast it.
+
+
+
+                Console.WriteLine("Hello World!");
+        }
+    }
+

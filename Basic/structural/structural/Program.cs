9416a18ccf64afd9bbe40f37d323e874ad5b8aed
ahmednr-dev <ahmednr.dev@gmail.com>
2021-04-25 20:15:11 +0200

    c-sharp structure

---
+++ Basic/structural/structural/Program.cs
@@ -0,0 +1,17 @@
+﻿//using System; // NameSpace Declration  indicate that you're using System namespace 
+
+//A namespace is used to organize your code and is collection of classes, interfaces, structs, enums and delegates.
+
+class Program
+    {
+    // Main method is the entry point into your application 
+    static void Main(string[] args)
+        {
+            // write to console 
+           Console.WriteLine("Hello World!"); //  Console class in namespace system 
+            // if you omit namespace declartion 
+            // you write full qualified name of the Console class
+            System.Console.WriteLine("Hello World!"); 
+        }
+
+    }

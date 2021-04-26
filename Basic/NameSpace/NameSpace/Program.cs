061abf3e3529ce0defeea3c7158f262bc04b171d
ahmednr-dev <ahmednr.dev@gmail.com>
2021-04-26 06:25:55 +0200

    NameSpaces

---
+++ Basic/NameSpace/NameSpace/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+// NameSpace is away to organize code , and avoid clashes ()
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        // invoke Print method from TeamA,B
+        ProjectA.TeamA.ClassA.Print();
+        ProjectA.TeamB.ClassB.Print();
+        }
+    }
+
+
+// The project can contain many NameSpace 
+
+namespace ProjectA
+{
+    namespace TeamA
+    {
+        class ClassA
+        {
+            public static void Print()
+            {
+                Console.WriteLine("Team A Print Method");
+            }
+        }
+    }
+
+}
+namespace ProjectA
+{
+    namespace TeamB
+    {
+        class ClassB
+        {
+            public static void Print()
+            {
+                Console.WriteLine("Team B Print Method");
+            }
+        }
+    }
+
+}

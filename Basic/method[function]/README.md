e2c0a2788cf4bd3bace2c8810aa00ef72c48b4c4
ahmednr-dev <ahmednr.dev@gmail.com>
2021-04-26 04:35:03 +0200

    static v.s instance method

---
+++ Basic/method[function]/README.md
@@ -0,0 +1,20 @@
+
+# Static V.S instance Method
+
+
+## Static 
+* when a method declaration includes a static modifiera method is said to be a static method
+
+* invoked using the Class name 
+
+* there is no instance of that method and you can invoke only the one definiation of the static method 
+## Instance Method
+
+* when no static modifier is present , the method is said to be  an instance method 
+
+* invoked using an instance of the class 
+
+* the multiple instance of a class can be created ( or instantiated ) and each instance has its own separate method
+
+
+
---
+++ Basic/method[function]/method[function]/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+    class Program
+
+    {
+    // static method v.s instance method [ that not contain static keyword ] 
+
+    static void Main(string[] args)
+        {
+          //Add(5,4); // Return error cuz method not static
+                    // An object reference is required for non-static method 
+       // Create object 
+        Program NewObject = new Program();
+        NewObject.Add(5,4);
+
+        // === if method static ===
+
+        //NewObject.Subtracting(5,2);  //accessed by type name [class name]
+        Program.Subtracting(5, 2);
+
+        }
+
+
+
+
+    // Method[akn function ] structure
+    // access-modifiers [public -priviate..etc ]  , return type[anyDataType or Void = absence of datatype]  , methodName , Parameters[optional]
+    /*
+        Method Body 
+     */
+
+    // Define instance method 
+    public int Add(int firstNum,int secondNum)
+    {
+        return firstNum + secondNum;
+    }
+
+    // define static method
+    public static int Subtracting (int firstNum,int secondNum)
+    {
+        return firstNum - secondNum;
+    }
+
+    }

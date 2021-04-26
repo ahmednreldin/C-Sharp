639ff26621e467bee47b1863f12b3679c5bc2a0a
ahmednr-dev <ahmednr.dev@gmail.com>
2021-04-26 02:27:12 +0200

    Comment

---
+++ Basic/Comments/Comments/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // SingleLine Comment // 
+        // MultiLine Comment  /* */
+        //xml documentation Commend ///
+    }
+
+    // sampleClass // if you put cursor on name of class will display xml doc
+}
+
+/// <summary>
+///  This is a Sample Class
+/// </summary>
+public class sampleClass
+{
+
+}
+

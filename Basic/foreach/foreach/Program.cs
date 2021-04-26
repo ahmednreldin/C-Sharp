cb1074c0ad9bcde7ea45bdc79a74688aec1ec8d9
ahmednr-dev <ahmednr.dev@gmail.com>
2021-04-26 03:35:56 +0200

    Foreach

---
+++ Basic/foreach/foreach/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+    class Program
+{
+    static void Main(string[] args)
+    {
+        // foreach deal with collection of iteams that has same data types 
+
+        int[] number = { 1, 2, 3, 4, 5, 6 };
+
+        // define variable as same data type for collection 
+        // compile will go to collection of numbers 
+        // take first value and put it in => i
+        // until collection finish 
+        foreach (int i in number)
+            Console.WriteLine(i);
+        
+        // benefit of foreach we don't deal with index 
+    }
+}

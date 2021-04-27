e1a460138fc38643dc5a62a720c3cbf43b89da6b
ahmednr-dev <ahmednr.dev@gmail.com>
2021-04-27 05:23:46 +0200

    Interfaces

---
+++ Basic/Interfaces/Interfaces/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+/* 
+    we create interfaces using interface keyword , just like classes interfaces also can contains 
+    [ properites , methods , delegates , events ]
+    - but only declarations and no implementation 
+    
+    its a compile error to provide implementation for any interface member 
+    
+    - interfaces member are public by defualt 
+    - interfaces cannot contain fields
+    - if a [class - struct ] inherits from an interface 
+=> it must provide implementation for all interface members , otherwise we get a compiler error 
+    - A [ class,struct ] can inherit from more than one interface at the same time 
+=> note a class cannot inhirt from more than once class at the same time 
+    - Interfaces can inheirt from other interfaces , A Class that inherits this interface must provide 
+implementation for all interface members in the intire interface inheritance chain 
+
+we cannot create an instance of an interface 
+=> but an interface reference variable can point to an derived class object 
+
+==> Interface Naming Conversion Interface names a prefixed with Capital I
+ */
+
+interface IStudent
+{
+    public void Print();
+
+    // int _id; // interfaces cannot contain fields
+}
+// FirstYear doesn't implement interface members
+/*class FirstYear : IStudent
+{
+}*/
+
+class FirstYear : IStudent
+{
+    public void Print()
+    { 
+    }
+}
+//   - A [ class,struct ] can inherit from more than one interface at the same time 
+
+interface I1 { } 
+interface I2 { }
+class Employee : I1, I2 { };
+//- Interfaces can inheirt from other interfaces , A Class that inherits this interface must provide 
+//implementation for all interface members in the intire interface inheritance chain 
+
+interface IOne {
+    void Print1();
+} 
+interface ITwo { 
+    void Print2();
+}
+
+class Numbers : IOne, ITwo
+{
+    public void Print1()
+    {
+    }
+
+    public void Print2()
+    {
+    }
+}
+
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+        //we cannot create an instance of an interface 
+        //  I1 newInterface = new I1();
+        // but an interface reference variable can point to an derived class object 
+        I1 newInterface = new Employee();
+        
+    
+    }
+
+    }
---
+++ Basic/StructVsClass/StructVsClass/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/* Class V.s Structs 
+ * 
+ * A Struct is 
+ * a value type
+ * stored in stack
+ * value type hod their value in memory where they are declared
+ * value type are destoryed immediately after the scope is lost 
+ * when copy struct to another they will have different references in memory  
+ * cannot have destructors
+ * cannot have explicit parameter less constructor Employee()
+ * cannot inherit from another class or struct (called sealed )
+ * can inheriant from interface 
+
+ * A class is
+ * a reference type 
+ * stored in heap 
+ * reference type hold a refrence to an object in memory
+ * refrence type variable is destoryed after the scope is lost , the object is later destroyed by garbage collector
+ * when copy class to another will have same reference which mean change in one will effect on another
+ * class can have destructors  
+ */
+
+
+namespace StructVsClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

aef82fecb8cc25af430a3420fd5126c85bd1ed81
ahmednr-dev <ahmednr.dev@gmail.com>
2021-04-28 01:32:08 +0200

    Enum

---
+++ Basic/Enum/Enum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        Customer c1 = new Customer
+        {
+            Gender = Gender.Male,
+        };
+
+        Console.WriteLine(c1.Gender);
+        
+        }
+    }
+
+public enum Gender
+{
+    Male,
+    Female,
+    Unknown,
+}
+
+public class Customer
+{
+    public Gender Gender { get; set; }
+}
---
+++ Basic/exceptionHandling/exceptionHandling/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+        StreamReader sr = null;
+        try
+        {
+             sr = new StreamReader(@"D:\C#\data.txt");
+
+            string readAllFile = sr.ReadToEnd();
+
+            Console.WriteLine(readAllFile);
+        }
+        catch (Exception ex) {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(ex.StackTrace);
+        }
+        finally
+        {   // Clean&free resources that the class was holding onto during the program execution , finally block gurnanted to execute whether there exception or not 
+            if (sr != null)
+            {
+                sr.Close();
+            }
+        }
+        }
+    }

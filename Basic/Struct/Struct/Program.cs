1d98a4b93add959eeb64ad847f9f9b874f6545d2
ahmednr-dev <ahmednr.dev@gmail.com>
2021-04-27 04:49:19 +0200

    Struct

---
+++ Basic/Struct/Struct/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+/* 
+    == Structs == 
+just like clasess structs can have
+1- private Fields 
+2- Public Properties
+3- Constructors 
+4 - Methods 
+*/
+
+struct Student
+{
+
+    // Fields ( private ) 
+    private int _id;
+    private string _name;
+
+    // Properties
+    public int Id { get => _id; set => _id = value; }
+    public string Name { get => _name; set => _name = value; }
+
+    // Constructor 
+    public Student(int id , string name)
+    {
+        this._id = id;
+        this._name = name;
+    }
+
+    // Methods
+    public void print()
+    {
+        Console.WriteLine("Student ID {0} \nName{1}", this._id, this._name);
+    }
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        Student s1 = new Student(1, "Ahmed");
+        Student s2 = new(2, "Mo");
+        s2.print();
+
+        // object initializer syntax
+        Student s3 = new Student{
+            Id = 3,
+            Name = "Hossam"
+        };
+
+    }
+    }

7159a44d523c0b2bfef15dfffd384db63ef6dbfc
ahmednr-dev <ahmednr.dev@gmail.com>
2021-04-27 02:08:01 +0200

    Inheritance

---
+++ Basic/MethodHiding/MethodHiding/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+/*
+        use the new keyword to hide a base class member 
+
+        == Different way to invoke a hidden base(parent) class member form derived(child) class
+  
+    1- Use base keyword()
+    2- cast child type to parent type and invoke hidden member 
+    3- ParentClass PC = new ChildClass(); PC.HiddenMethod()
+ */
+
+class Employee {
+    public string fname, lname;
+
+    public void Print()
+    {
+        Console.WriteLine(fname + lname);
+    }
+}
+class FullTimeEmployee : Employee{
+
+    /// <summary>
+    /// Compilation error 
+    /// use new keyword if hidden was intended 
+    /// </summary>
+    /*public void Print()
+    {
+        Console.WriteLine(fname + lname);
+    }*/
+
+
+    /*public new void Print()
+    {
+        Console.WriteLine(fname + lname + "Full Time Employee");
+    }*/
+
+    // Base method to can invoke hidden method 
+    /* public new void Print()
+     {
+         base.Print(); 
+         Console.WriteLine(fname + lname + "Full Time Employee"); // execute both function if we didnt comment this 
+     }
+ */
+
+    // cast type method
+    public new void Print()
+    {
+        Console.WriteLine(fname + lname + "Full Time Employee");
+    }
+}
+class PartTimeEmployee { }
+class Program
+    {
+        static void Main(string[] args)
+        {
+        // invoke hidden method
+        FullTimeEmployee FTM = new FullTimeEmployee();
+        FTM.fname = "Ahmad";
+        FTM.lname = "Nour";
+        FTM.Print();
+
+        // Base method 
+        //FTM.Print();
+
+        // cast method
+        ((Employee)FTM).Print();
+
+        // Pointing to base reference 
+        Employee FTMTB = new FullTimeEmployee();
+        // in this case will invoke bais class and the method inside it 
+        // this consider Polymorphism concept  object(bais reference variable ) pointing to drived(child) class 
+        // FTMB of type employee poining to adderss from type FTE
+        FTMTB.fname = "Ahmad";
+        FTMTB.lname = "Nour";
+        FTMTB.Print();
+
+    }
+}
---
+++ Basic/inheritance/inheritance/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+/* 
+        Inheritance 
+    - one of praimary paillars of oop
+    - allows code reuse
+    - code reuse can reduce time(for testing & writing dublicate code ) and erros 
+  
+    == Note ==
+
+- c# supports only single class inheritance 
+- c# supports multiple inferface inhertitance 
+- Parant(base) classes are autmoatically instantiated before child(derived classes 
+- Parent Class constructor executes before child class constructor 
+ 
+ */
+class Employee
+{
+    public string fname;
+    public string lname;
+    public string email;
+
+    public void print()
+    {
+        Console.WriteLine("Employee Name {0} \nEmployee Email {1}", fname+lname, email);
+    }
+}
+
+class FullTimeEmployee : Employee
+{
+    float yearlySalary;
+}
+
+
+class PartTimeEmployee : Employee
+{
+    float hourlyRate;
+}
+
+//-c# supports only single class inheritance 
+
+class A { }
+
+//class B : A, Employee { }; // class B cannot have multiple base(parant) classes
+
+//- c# supports multiple inferface inhertitance [ Multiple level class inheritance ] have same parant 
+// this mean class C inherant from FullTimeEmployee and FTE inherant from Employee 
+class C : FullTimeEmployee { };
+
+
+
+//-Parent Class constructor executes before child class constructor
+// you can avoid this using base keyword 
+
+class Parant {
+     public Parant()
+    {
+        Console.WriteLine("Parant class Constructor Called");
+    } 
+    public Parant(string Message)
+    {
+        Console.WriteLine(Message);
+    }
+
+
+};
+
+// base force the constructor used instead of called less paramerers consturctor
+class Child : Parant
+{
+    public Child() : base("Child class controlling ")
+    {
+        Console.WriteLine("Child class Constructor Called ");
+    }
+}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Employee lead = new Employee();
+        lead.fname = "Ahmed";
+        lead.lname = "Nour";
+        lead.email = "ahmednr.dev@gmail.com";
+        lead.print();
+
+        FullTimeEmployee fte = new FullTimeEmployee();
+        fte.fname = " Mohamed ";
+        fte.lname = " Nour ";
+        fte.email = "blahblah@blab.co";
+        fte.print();
+
+        // child 
+        Child ch1 = new Child();
+        }
+    }

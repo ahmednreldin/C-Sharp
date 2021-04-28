f9884764f64b982d06fa24c56e05828eb7566cc7
ahmednr-dev <ahmednr.dev@gmail.com>
2021-04-28 03:31:22 +0200

    generics

---
+++ Basic/Generics/Generics/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+// Generics is similar to Templates in c++
+// allow us to design class&method decoupling from the data types 
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AreEqual<int>(4, 4));
+            Console.WriteLine(AreEqual<string>("a", "A"));
+            
+        }
+
+        // using boxing&unboxing ( object )  value type => reference type 
+        // disAdvantage isNot A Type safe mean can pass AreEqual(1,"2") strongly typed property lose
+        // performance intensive due to boxing and unboxing   
+        public static bool AreEqual(object value1, object value2)
+        {
+            return value1.Equals(value2);
+        }
+        // using Generic <T> 
+        public static bool AreEqual<T>(T value1,T value2)
+        {
+            return value1.Equals(value2);
+        }
+    }
+}
